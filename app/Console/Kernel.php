06001d43a17b710547ec3a90f2b1e1b4f6e537f5
Hemant <rj.hemantjangid@gmail.com>
2020-10-25 22:25:25 +0530

    commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\UpdatePrice::class,
     ];
 
     /**
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('price:cron')->everyMinute();
         /*$schedule->call(function(){
             Admin::create([
                 'name' => "Demoasas",
